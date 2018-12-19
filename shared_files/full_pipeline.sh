13b637dac97f5d2bea87173620e8989212ce44f9
Jadwiga Pokorska <jadwiga.m.pokorska@gmail.com>
2018-12-19 12:04:44 +0100

    New hello worlds + recompile/multifile direct run etc. functionality in full_pipeline script.

--- shared_files/full_pipeline.sh
+++ shared_files/full_pipeline.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 RUN=false
+TRANSLATE=true
 DEBUG=false
-OUTPUT="output.pda"
+CLEAN=false
+OUTPUT="output/base"
 POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
@@ -27,6 +29,17 @@ case $key in
     DEBUG=true
     shift
     ;;
+    --recompile|--clean)
+    CLEAN=true
+    shift
+    ;;
+    -d|--direct)
+    TRANSLATE=false
+    RUN=true
+    OUTPUT="$2"
+    shift
+    shift
+    ;;
     *)    # unknown option
     POSITIONAL+=("$1") # save it in an array for later
     shift # past argument
@@ -36,34 +49,45 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 cd ../bf_parser/;
+if [ $CLEAN = true ]; then make clean; fi
 make all;
 cp test.e ../shared_files/bf_to_tm.e;
 
 cd ../tm_parser/;
+if [ $CLEAN = true ]; then make clean; fi
 make all;
 cp test.e ../shared_files/tm_to_pda.e;
 
 cd ../2stackPDA_parser/;
+if [ $CLEAN = true ]; then make clean; fi
 make all;
 cp test.e ../shared_files/pda_to_cm4.e;
 
 cd ../mm4_parser/;
+if [ $CLEAN = true ]; then make clean; fi
 make all;
 cp test.e ../shared_files/cm4_to_cm2.e;
 
 cd ../shared_files/;
 
-./bf_to_tm.e -turing $FILE > tmp.code;
-./tm_to_pda.e -2StackPDA tmp.code > tmp2.code
-./pda_to_cm4.e -translateTo $OUTPUT tmp2.code
+if [ $TRANSLATE = true ]; then
+  DIRNAME=`dirname $OUTPUT`;
+  if [ -d $DIRNAME ]; then
+    echo "Output directory $DIRNAME already exists. I'm removing it.";
+    rm -Rf $DIRNAME;
+  fi
+  mkdir $DIRNAME;
+  ./bf_to_tm.e -turing $FILE > tmp.code;
+  ./tm_to_pda.e -2StackPDA tmp.code > tmp2.code
+  ./pda_to_cm4.e -multifile "$OUTPUT" tmp2.code
+fi
 
 if [ $RUN = true ]; then
   echo "> Running the translated code";
   if [ $DEBUG = true ]; then
-    ./cm4_to_cm2.e --debug $OUTPUT;
+    ./cm4_to_cm2.e --debug -multifile "$OUTPUT";
   else
-    ./cm4_to_cm2.e $OUTPUT;
+    ./cm4_to_cm2.e -multifile "$OUTPUT";
   fi
 fi
-
 rm bf_to_tm.e tm_to_pda.e pda_to_cm4.e cm4_to_cm2.e tmp.code tmp2.code
